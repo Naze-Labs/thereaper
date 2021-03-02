6d569c1078517314e6e203eb36fff4b5cba24f20
omoniyi22 <omoniyioluwaseun22gmail.com>
2021-03-02 02:25:06 +0100

    Instructor integrated and auth api consumed

--- stores/NewEgg/bot.js
+++ stores/NewEgg/bot.js
@@ -10,7 +10,7 @@ module.exports = function(callback, input) {
     let status;
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox"]
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
     });
     const page = await browser.newPage();
     await page.setCacheEnabled(false);
